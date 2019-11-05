a632eddbdc20d4824922d6a412dd85ba85e87c18
Aluno <Aluno@SSA-D-CPIL03-21.intranet.cefetba.br>
2019-11-05 20:57:07 -0300

    Classes Porta e ContadorVolta

---
+++ PortaContador/ContadorVolta.java
@@ -0,0 +1,17 @@
+class ContadorVolta{
+    int valor;
+    
+    void contar(){
+        valor = valor + 1;
+    }    
+    
+    void zerar(){
+        valor = 0;
+    }    
+    
+    int buscarValor(){
+        return valor;
+    }    
+    
+    
+}
---
+++ PortaContador/Porta.java
@@ -0,0 +1,18 @@
+class Porta{
+    boolean aberta;
+    
+    void abrir(){
+      aberta = true;  
+    };    
+    
+    void fechar(){
+      aberta = false;  
+    };    
+    
+    
+    boolean estaAberta(){
+        return aberta;
+    };    
+    
+    
+};
